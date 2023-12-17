5923f23ee8a08fbb2ac43d8a0c6b0ec23d0eb858
Hardik Anand <hardikanand633@gmail.com>
2023-12-17 22:49:09 +0530

    Database(SQL) Connection

--- routes/route.go
+++ routes/route.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 
 	"gofr.dev/pkg/gofr"
 )
@@ -17,11 +18,11 @@ var db *sql.DB
 
 func main() {
 	// Connect to your database
-	//conn, err := initDatabase()
-	/*if err != nil {
+	conn, err := initDatabase()
+	if err != nil {
 		fmt.Println("Error initializing database:", err)
 
-	}*/
+	}
 	//db = conn
 	defer db.Close()
 
@@ -33,4 +34,22 @@ func main() {
 	//app.DELETE("/healthify/{id}", deleteStudent)
 
 	app.Start()
+}
+func initDatabase() (*sql.DB, error) {
+
+	host := "localhost"
+	port := "3306"
+	user := "root"
+	password := "zopsmart123#"
+	database := "health"
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", user, password, host, port, database)
+	var err error
+	db, err = sql.Open("mysql", dsn)
+	if err != nil {
+		fmt.Println("error validating sql.open")
+		return nil, err
+	}
+
+	return db, nil
 }
